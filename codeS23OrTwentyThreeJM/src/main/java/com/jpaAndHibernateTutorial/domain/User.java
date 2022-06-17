dc8330c6c9b9071b5704339c375b595850619a79
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-17 22:09:07 +0430

    edit User

--- codeS23OrTwentyThreeJM/src/main/java/com/jpaAndHibernateTutorial/domain/User.java
+++ codeS23OrTwentyThreeJM/src/main/java/com/jpaAndHibernateTutorial/domain/User.java
@@ -86,6 +86,15 @@ public class User {
         this.password = password;
     }
 
-
-
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
 }
