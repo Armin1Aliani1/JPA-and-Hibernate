e73915c0ba920cf54cf6410a72713784e4705765
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-17 16:30:50 +0430

    edit HibernateUtil

--- codeS23OrTwentyThreeJM/src/main/java/com/jpaAndHibernateTutorial/util/HibernateUtil.java
+++ codeS23OrTwentyThreeJM/src/main/java/com/jpaAndHibernateTutorial/util/HibernateUtil.java
@@ -4,9 +4,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class HibernateUtil {
-    private static final EntityManagerFactory emf /*emf = Persistence.createEntityManagerFactory("default")*/;
+    private static final EntityManagerFactory emf;
+//    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
     static {
         emf = Persistence.createEntityManagerFactory("default");
+//        emf = Persistence.createEntityManagerFactory("name file persistence in tag <persistence-unit name="default">");
     }
     public static EntityManagerFactory getEntityManagerFactory(){
         return emf;
