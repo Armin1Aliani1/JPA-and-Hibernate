05eea58ba51c390b2b5fdf70705693afea471dee
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-17 22:09:41 +0430

    edit JpaApplication

--- codeS23OrTwentyThreeJM/src/main/java/com/jpaAndHibernateTutorial/JpaApplication.java
+++ codeS23OrTwentyThreeJM/src/main/java/com/jpaAndHibernateTutorial/JpaApplication.java
@@ -1,12 +1,26 @@
 package com.jpaAndHibernateTutorial;
 
+import com.jpaAndHibernateTutorial.domain.User;
 import com.jpaAndHibernateTutorial.util.HibernateUtil;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class JpaApplication {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = HibernateUtil.getEntityManagerFactory();
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        TypedQuery<User> query = entityManager.createQuery("SELECT u from User u", User.class);
+        List<User> userList = query.getResultList();
+        if (userList==null||userList.isEmpty()){
+            System.out.println("Empty users");
+        }else {
+            userList.forEach(user -> System.out.println(user));
+        }
+
         System.out.println("done");
     }
 }
